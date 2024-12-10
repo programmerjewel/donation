2d5022c690c1c9fdb9bc24c1ea328bf9a838ef2e
programmerjewel <jewelruet1402@gmail.com>
2024-12-10 22:08:48 +0600

    html updated

--- main.js
+++ main.js
@@ -11,16 +11,18 @@ function showSections(activeSection, btnActive, btnDeactive){
 
     activeSection.classList.remove('hidden');
     
-    //change button properties based on active
+    //change button properties based on active and deactive
     btnActive.style.backgroundColor = '#B4F461';
     btnActive.style.borderColor = '#B4F461';
     btnDeactive.style.backgroundColor = 'white';
     btnDeactive.style.borderColor = '#E5E7EB';
 }
 
+// active donation and history button
 donationBtn.addEventListener('click', function(){
     showSections(donateSection, donationBtn, historyBtn);
 });
+
 historyBtn.addEventListener('click', function(){
     showSections(historySection, historyBtn, donationBtn);
 });
@@ -28,11 +30,12 @@ historyBtn.addEventListener('click', function(){
 
 //function about calculate donate amount
 function donateCalculate(mainBalanceById, inputValueById, categoryAmountId, header){
+
     const mainBalance = parseFloat(document.getElementById(mainBalanceById).innerText);
     const inputAmount = parseFloat(document.getElementById(inputValueById).value);
     const categoryAmount = parseFloat(document.getElementById(categoryAmountId).innerText);
     
-    //donate input value validation
+    //donate-input value validation
     if (inputAmount > mainBalance){
         alert('Your input amount cannot be bigger than the main balance amount');
         return;
@@ -42,7 +45,7 @@ function donateCalculate(mainBalanceById, inputValueById, categoryAmountId, head
         return;
     }
 
-    //calculations
+    //calculations for main balance and category amount
     document.getElementById(mainBalanceById).innerText = mainBalance - inputAmount;
     document.getElementById(categoryAmountId).innerText =  categoryAmount + inputAmount;
 
@@ -54,7 +57,7 @@ function donateCalculate(mainBalanceById, inputValueById, categoryAmountId, head
     addHistory(inputAmount, header);
 }
 
-
+//function fot history section design with innerHTML
 function addHistory(amount, header){
     const date = Date();
     const donateDiv = document.createElement('div');
@@ -63,20 +66,24 @@ function addHistory(amount, header){
         <h3 class="font-semibold text-lg mb-2">${amount} taka donated for ${header}</h3>
         <p class="font-light text-[#111111b2]">${date}</p>
     `;
+    //append generated div to history-section
     historySection.appendChild(donateDiv);
 }
 
 
-
 //add click event to all donate button to calculate donation amount
+
+//calculate total donate amount for noalkahli flood relief
 document.getElementById('n-btn').addEventListener('click', function(){
-    donateCalculate('main-balance', 'n-input', 'n-amount', 'Support Noakhali Flood Relief Efforts, Bangladesh');
+    donateCalculate('main-balance', 'n-input', 'n-amount', 'support Noakhali Flood Relief Efforts, Bangladesh');
 });
+//calculate total donate amount for feni flood relief
 document.getElementById('f-btn').addEventListener('click', function(){
-    donateCalculate('main-balance', 'f-input', 'f-amount', 'Support Feni Flood Relief Efforts, Bangladesh');
+    donateCalculate('main-balance', 'f-input', 'f-amount', 'support Feni Flood Relief Efforts, Bangladesh');
 });
+//calculate total donate amount for quota movement protest
 document.getElementById('q-btn').addEventListener('click', function(){
-    donateCalculate('main-balance', 'q-input', 'q-amount', 'Aid of Injured in the Quota Movement, Bangladesh');
+    donateCalculate('main-balance', 'q-input', 'q-amount', 'aid of Injured in the Quota Movement, Bangladesh');
 });
 
 //go to blog.html page
